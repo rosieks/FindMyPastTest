b56f422376c85750f60955e6e55cd37bd401b50f
rosieks <rosieks@hotmail.com>
2015-10-31 13:35:07 +0100

    Naive implementation of prime number generator

--- FindMyPastTest/PrimeNumberGenerator.cs
+++ FindMyPastTest/PrimeNumberGenerator.cs
@@ -11,7 +11,39 @@ namespace FindMyPastTest
 
         public IEnumerable<int> Generate()
         {
-            throw new NotImplementedException();
+            int i = 1;
+            while (true)
+            {
+                i++;
+                if (IsPrime(i))
+                {
+                    yield return i;
+                }
+            }
+        }
+
+        private bool IsPrime(int n)
+        {
+            if (n == 2)
+            {
+                return true;
+            }
+
+            if (n == 1 || n % 2 == 0)
+            {
+                return false;
+            }
+
+            int sqrt = (int)Math.Sqrt(n);
+            for (int i = 3; i <= sqrt; i += 2)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
