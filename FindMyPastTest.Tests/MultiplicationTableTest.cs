87359da60e202f77121f830deedfb84e473571b4
rosieks <rosieks@hotmail.com>
2015-10-31 13:56:22 +0100

    Added tests for multiplication table

---
+++ FindMyPastTest.Tests/MultiplicationTableTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FindMyPastTest.Tests
+{
+    public class MultiplicationTableTest
+    {
+        [Fact]
+        public void ShouldGenerateMultiplicationTable()
+        {
+            var expected = new[]
+            {
+                new { Row = 0, Column = 0, Result = 4 },
+                new { Row = 0, Column = 1, Result = 6 },
+                new { Row = 0, Column = 2, Result = 10 },
+                new { Row = 1, Column = 0, Result = 6 },
+                new { Row = 1, Column = 1, Result = 9 },
+                new { Row = 1, Column = 2, Result = 15 },
+                new { Row = 2, Column = 0, Result = 10 },
+                new { Row = 2, Column = 1, Result = 15 },
+                new { Row = 2, Column = 2, Result = 25 },
+            };
+
+            var target = new MultiplicationTable(new[] { 2, 3, 5 });
+            foreach (var item in expected)
+            {
+                Assert.Equal(item.Result, target[item.Column, item.Row]);
+            }
+        }
+
+        [Fact]
+        public void ShouldReturnHeaders()
+        {
+            var target = new MultiplicationTable(new[] { 2, 3, 5 });
+
+            Assert.Equal(2, target.Header[0]);
+            Assert.Equal(3, target.Header[1]);
+            Assert.Equal(5, target.Header[2]);
+        }
+    }
+}
---
+++ FindMyPastTest/MultiplicationTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FindMyPastTest
+{
+    public class MultiplicationTable
+    {
+        private int[] v;
+
+        public MultiplicationTable(int[] v)
+        {
+            this.v = v;
+        }
+
+        public int this[int column, int row]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int[] Header
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
