f8d0e47d9c6c432cf2370db9df7c7ceb42de56ef
rosieks <rosieks@hotmail.com>
2015-10-31 13:20:24 +0100

    Added primenumbergenerator class draft with failing test

---
+++ FindMyPastTest.Tests/PrimeNumberGeneratorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FindMyPastTest.Tests
+{
+    public class PrimeNumberGeneratorTest
+    {
+        [Fact]
+        public void ShouldGenerateFirst10PrimeNumbers()
+        {
+            var expected = new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 31 };
+            var target = new PrimeNumberGenerator();
+
+            var actual = target.Generate().Take(10).ToArray();
+
+            Assert.Equal(expected.Length, actual.Length);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+        }
+    }
+}
---
+++ FindMyPastTest/PrimeNumberGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindMyPastTest
+{
+    public class PrimeNumberGenerator
+    {
+        public PrimeNumberGenerator()
+        {
+        }
+
+        public IEnumerable<int> Generate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
