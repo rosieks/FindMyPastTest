6317f9ee1f1c0e43ca3511271d7f83fb04ecc7cf
rosieks <rosieks@hotmail.com>
2015-10-31 14:39:44 +0100

    Added missing file

--- FindMyPastTest/Program.cs
+++ FindMyPastTest/Program.cs
@@ -10,6 +10,21 @@ namespace FindMyPastTest
     {
         static void Main(string[] args)
         {
+            Console.Write("How many prime number do you want to generate: ");
+            string numberText = Console.ReadLine();
+            int number;
+            while (!int.TryParse(numberText, out number))
+            {
+                Console.Write("Invalid format. Try again: ");
+                numberText = Console.ReadLine();
+            }
+
+            var generator = new PrimeNumberGenerator();
+            var table = new MultiplicationTable(generator.Generate().Take(number).ToArray());
+            var writer = new MultiplicationTableWriter();
+            writer.Write(Console.Out, table);
+
+            Console.Read();
         }
     }
 }
