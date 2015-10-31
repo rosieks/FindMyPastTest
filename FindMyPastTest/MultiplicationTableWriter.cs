2700208ee5bfa9583246bcbeeba192db7bb26d4f
rosieks <rosieks@hotmail.com>
2015-10-31 14:34:09 +0100

    Initial implementation of multiplication table writer

--- FindMyPastTest/MultiplicationTableWriter.cs
+++ FindMyPastTest/MultiplicationTableWriter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace FindMyPastTest
 {
@@ -11,7 +12,17 @@ namespace FindMyPastTest
 
         public void Write(StringWriter textWriter, MultiplicationTable multiplicationTable)
         {
-            throw new NotImplementedException();
+            int maxNumber = multiplicationTable[multiplicationTable.Header.Length - 1, multiplicationTable.Header.Length - 1];
+            int width = maxNumber.ToString().Length;
+
+            var header = new[] { "" }.Concat(multiplicationTable.Header.Select(x => x.ToString())).Select(x => x.PadLeft(width));
+            textWriter.WriteLine("| {0} |", string.Join(" | ", header));
+            textWriter.WriteLine("| {0} |", string.Join(" | ", Enumerable.Range(0, multiplicationTable.Header.Length + 1).Select(i => "".PadLeft(width, '-'))));
+            for (int i = 0; i < multiplicationTable.Header.Length; i++)
+            {
+                var rowNumber = new[] { multiplicationTable.Header[i] }.Concat(multiplicationTable.Header.Select((x, j) => multiplicationTable[i, j]));
+                textWriter.WriteLine("| {0} |", string.Join(" | ", rowNumber.Select(x => x.ToString().PadLeft(width))));
+            }
         }
     }
 }
