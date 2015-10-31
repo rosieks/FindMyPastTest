091a567672e1fef8540d733cfd068f8fb79c4da6
rosieks <rosieks@hotmail.com>
2015-10-31 14:08:00 +0100

    Added test for multiplication table writer

---
+++ FindMyPastTest.Tests/MultiplicationTableWriterTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FindMyPastTest.Tests
+{
+    public class MultiplicationTableWriterTest
+    {
+        [Fact]
+        public void ShouldWriteMultiplicationTableToConsole()
+        {
+            var expected =
+            "|    |  2 |  3 |  5 |\r\n" +
+            "|----|----|----|----|\r\n" +
+            "|  2 |  4 |  6 | 10 |\r\n" +
+            "|  3 |  6 |  9 | 15 |\r\n" +
+            "|  5 | 10 | 15 | 25 |\r\n";
+
+            var textWriter = new StringWriter();
+            var target = new MultiplicationTableWriter();
+            target.Write(textWriter, new MultiplicationTable(new[] { 2, 3, 5 }));
+
+            Assert.Equal(expected, textWriter.ToString());
+        }
+    }
+}
---
+++ FindMyPastTest/MultiplicationTableWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace FindMyPastTest
+{
+    public class MultiplicationTableWriter
+    {
+        public MultiplicationTableWriter()
+        {
+        }
+
+        public void Write(StringWriter textWriter, MultiplicationTable multiplicationTable)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
